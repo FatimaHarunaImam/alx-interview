d3db8171e0b6521ce77b316337da98d567b566b9
FatimaHarunaImam <101684307+FatimaHarunaImam@users.noreply.github.com>
2024-10-28 00:19:11 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,51 @@
 #!/usr/bin/python3
-
+"""This module parses a log that's displayed
+in stdin and displays its stats.
+"""
 import sys
+import re
 
 
-def print_msg(dict_sc, total_file_size):
+def printDict(total_size, occurenceDict):
+    """ This function prints the dictionary of
+        Status code occurrences in the parsed log
+        in sorted order.
+        Args:
+            occurenceDict (dict): the dictionary of occurrences.
     """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+    print("File size: {}".format(total_size))
+    for key in sorted(occurenceDict.keys()):
+        if occurenceDict[key] != 0:
+            print("{}: {}".format(key, occurenceDict[key]))
 
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+count = 0
+total_size = 0
+occurenceDict = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
-
-        if len(parsed_line) > 2:
-            counter += 1
-
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
+        parsed = line.split()
+        if len(parsed) > 2:
+            count += 1
+            try:
+                statusCode = parsed[-2]
+                fileSize = int(parsed[-1])
+                if statusCode in occurenceDict.keys():
+                    occurenceDict[statusCode] += 1
+                total_size += fileSize
+                if count % 10 == 0:
+                    printDict(total_size, occurenceDict)
+            except Exception:
+                continue
 finally:
-    print_msg(dict_sc, total_file_size)
+    printDict(total_size, occurenceDict)
