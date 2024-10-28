0e42832b73b5af5530c7dc5a7c9add3658a2ea08
FatimaHarunaImam <101684307+FatimaHarunaImam@users.noreply.github.com>
2024-10-28 16:25:35 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
+"""
+ Calculates the fewest number of operations needed
+"""
+import math
+
+
+def lar_div(n):
+    """
+    returns largest divisor of n
+    """
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return n // i
+    return 1
 
 
 def minOperations(n):
-    if n <= 1:
+    """
+    minOperations
+    returns min operations needed to result in exactly n H characters
+    """
+    if n < 2:
         return 0
-    
-    operations = 0
-    factor = 2
-    
-    while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
-    
-    return operations
+    largest_divisor = lar_div(n)
+    return (n // largest_divisor) + minOperations(largest_divisor)
