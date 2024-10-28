e586f6f0d867e3165778163eb960d08440af28b7
FatimaHarunaImam <101684307+FatimaHarunaImam@users.noreply.github.com>
2024-10-28 16:27:58 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,37 @@
 #!/usr/bin/python3
-"""This module parses a log that's displayed
-in stdin and displays its stats.
-"""
+'''Reads stdin line by line and computes metrics'''
+
+
 import sys
-import re
 
-
-def printDict(total_size, occurenceDict):
-    """ This function prints the dictionary of
-        Status code occurrences in the parsed log
-        in sorted order.
-        Args:
-            occurenceDict (dict): the dictionary of occurrences.
-    """
-    print("File size: {}".format(total_size))
-    for key in sorted(occurenceDict.keys()):
-        if occurenceDict[key] != 0:
-            print("{}: {}".format(key, occurenceDict[key]))
-
-
-count = 0
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-occurenceDict = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0,
-}
+counter = 0
 
 try:
     for line in sys.stdin:
-        parsed = line.split()
-        if len(parsed) > 2:
-            count += 1
-            try:
-                statusCode = parsed[-2]
-                fileSize = int(parsed[-1])
-                if statusCode in occurenceDict.keys():
-                    occurenceDict[statusCode] += 1
-                total_size += fileSize
-                if count % 10 == 0:
-                    printDict(total_size, occurenceDict)
-            except Exception:
-                continue
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
 finally:
-    printDict(total_size, occurenceDict)
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
